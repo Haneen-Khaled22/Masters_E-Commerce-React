9bb18a3be963226b7372e66171492627b9c8316c
Haneen-Khaled22 <haneenk385@gmail.com>
2025-08-31 06:25:31 +0300

    sliders

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import HomeSlider from '../HomeSlider/HomeSlider'
+import BestSellers from '../Best sellers/BestSellers'
+import sidepanner1 from "../../assets/SidePanner1 (2).png"
+import sidepanner2 from "../../assets/SidePanner2 (2).png"
 
 
 function Home() {
@@ -6,8 +10,19 @@ function Home() {
     
     return (
         <div >
-            Home
-            {/* <HomeSlider/> */}
+            
+            <HomeSlider/>
+            <div className='mt-5 flex gap-3 max-w-7xl justify-center'>
+                <div className='w-1/4 ml-8 hidden lg:block'>
+                <img src={sidepanner1} alt='side panner' className='w-3/4 h-[400px] mt-4 mb-6 rounded-lg'/>
+                <img src={sidepanner2} alt='side panner' className='w-3/4 h-[400px] mt-4 rounded-lg'/>
+                
+                </div>
+                <div className='w-3/4'>
+                 <BestSellers/>
+                </div>
+            </div>
+           
             </div>
     )
 }
