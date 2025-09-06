50ce6a0338470a11c165d67ee0e5de474a005b3f
AbdallahAbozeid <abdallahabozeid10@gmail.com>
2025-09-06 14:43:09 +0300

    verify if user is loged in before adding to cart or wishlist

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,9 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useAuth } from "./AuthContext";
 
 const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  const { user } = useAuth();
   // ✅ استرجاع البيانات من localStorage أول مرة
   const [cart, setCart] = useState(() => {
     const saved = localStorage.getItem("cart");
@@ -26,6 +28,29 @@ export const CartProvider = ({ children }) => {
   }, [cart]);
 
   const addToCart = (id, price) => {
+    if (!user) {
+      toast((t) => (
+        <span className="flex gap-2 capitalize items-center">
+          <span className="text-brand-yellow">
+            <i className="fa-solid fa-warning"></i>
+          </span>
+          <a
+            className="text-brand-main underline uppercase font-semibold"
+            href="/login"
+          >
+            login
+          </a>{" "}
+          to add items to cart
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="text-brand-red"
+          >
+            <i className="fa-solid fa-close"></i>
+          </button>
+        </span>
+      ));
+      return;
+    }
     setCart((prev) => [...prev, { id, price, quantity: 1 }]);
     toast.success("Added to cart");
   };
--- src/Context/WishListContext.jsx
+++ src/Context/WishListContext.jsx
@@ -1,8 +1,14 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useAuth } from "./AuthContext";
+
 const WishListContext = createContext();
 
 export const WishListProvider = ({ children }) => {
+  const { user } = useAuth();
+
+
+
   // ✅ استرجاع البيانات من localStorage أول مرة
   const [wishList, setWishList] = useState(() => {
     const saved = localStorage.getItem("wishList");
@@ -15,6 +21,30 @@ export const WishListProvider = ({ children }) => {
   }, [wishList]);
 
   const addToWishList = (item) => {
+   if (!user) {
+      toast((t) => (
+        <span className="flex gap-2 capitalize items-center">
+          <span className="text-brand-yellow">
+            <i className="fa-solid fa-warning"></i>
+          </span>
+          <a
+            className="text-brand-main underline uppercase font-semibold"
+            href="/login"
+          >
+            login
+          </a>{" "}
+          to add items to wishList
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="text-brand-red"
+          >
+            <i className="fa-solid fa-close"></i>
+          </button>
+        </span>
+      ));
+      return;
+    }
+    
     setWishList((prev) => [...prev, item]);
     toast.success("Added to wishlist");
   };
