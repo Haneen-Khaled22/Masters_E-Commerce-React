db99ad58c929194a058a5674c0ecda3303ee6547
Haneen-Khaled22 <haneenk385@gmail.com>
2025-09-07 18:06:13 +0300

    padding

--- src/Components/Best sellers/BestSellers.jsx
+++ src/Components/Best sellers/BestSellers.jsx
@@ -93,7 +93,7 @@ function BestSellers() {
           <span className="loader"></span>
         </div>
       ) : (
-        <div className="best-sellers-slider  p-4 rounded-lg">
+        <div className="best-sellers-slider  rounded-lg">
           <Splide options={splideOptions} className="custom-splide w-full px-1 z-index-10">
             {bestSellers.map((p, index) => (
               <SplideSlide key={p.id}>
